e021263e8472fff464008798486392d23b6fe15a
Pankaj Gaikar <pankaj@orionlabs.io>
2025-11-01 11:58:41 +0530

    Initial project

---
+++ UniversalPercentageCalculator/UniversalPercentageCalculatorApp.swift
@@ -0,0 +1,17 @@
+//
+//  UniversalPercentageCalculatorApp.swift
+//  UniversalPercentageCalculator
+//
+//  Created by Pankaj Gaikar on 01/11/25.
+//
+
+import SwiftUI
+
+@main
+struct UniversalPercentageCalculatorApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ UniversalPercentageCalculatorTests/UniversalPercentageCalculatorTests.swift
@@ -0,0 +1,17 @@
+//
+//  UniversalPercentageCalculatorTests.swift
+//  UniversalPercentageCalculatorTests
+//
+//  Created by Pankaj Gaikar on 01/11/25.
+//
+
+import Testing
+@testable import UniversalPercentageCalculator
+
+struct UniversalPercentageCalculatorTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
